efb05b2602586b54d96d30c3d60cd27fd179eac8
Anh Anh <ngovietanh121@gmail.com>
2023-11-08 21:43:38 +0700

    21h43 11/8/2023
    upload file lên git

--- src/ui/DangNhapJFrame.java
+++ src/ui/DangNhapJFrame.java
@@ -14,6 +14,7 @@ import javax.swing.JOptionPane;
 import javax.swing.Timer;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.AbstractDocument;
 import jbdc.JBDCUtils;
 
 /**
@@ -88,9 +89,19 @@ public class DangNhapJFrame extends javax.swing.JFrame {
                 timer.stop();
             }
         });
-        
+
         //chạy timer ở trên
         timer.start();
+    }
+
+    public void showHideButton() {
+        if (ckbShowHide.isSelected()) {
+            txtMatKhau.setEchoChar((char) 0);
+            ckbShowHide.setText("Hide");
+        } else {
+            txtMatKhau.setEchoChar('*');
+            ckbShowHide.setText("Show");
+        }
     }
 
     /**
@@ -105,12 +116,13 @@ public class DangNhapJFrame extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         txtTenDangNhap = new javax.swing.JTextField();
         btnDangNhap = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        btnExit = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         txtMatKhau = new javax.swing.JPasswordField();
         lblCanhBao = new javax.swing.JLabel();
+        ckbShowHide = new javax.swing.JCheckBox();
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -124,10 +136,10 @@ public class DangNhapJFrame extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Thoát");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnExit.setText("Thoát");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnExitActionPerformed(evt);
             }
         });
 
@@ -143,6 +155,21 @@ public class DangNhapJFrame extends javax.swing.JFrame {
         txtMatKhau.setText("jPasswordField1");
 
         lblCanhBao.setForeground(new java.awt.Color(255, 0, 0));
+
+        ckbShowHide.setText("Show");
+        ckbShowHide.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ckbShowHideMouseClicked(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                ckbShowHideMousePressed(evt);
+            }
+        });
+        ckbShowHide.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                ckbShowHideKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -161,13 +188,14 @@ public class DangNhapJFrame extends javax.swing.JFrame {
                                 .addComponent(lblCanhBao, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)))
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnExit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addComponent(jLabel3)
                     .addComponent(jLabel4)
                     .addComponent(txtTenDangNhap)
                     .addComponent(txtMatKhau))
-                .addContainerGap(38, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(ckbShowHide, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -179,14 +207,16 @@ public class DangNhapJFrame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ckbShowHide))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel1)
                     .addComponent(lblCanhBao, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
+                    .addComponent(btnExit)
                     .addComponent(btnDangNhap))
                 .addGap(168, 168, 168))
         );
@@ -223,10 +253,22 @@ public class DangNhapJFrame extends javax.swing.JFrame {
         KiemTraDauVao();
     }//GEN-LAST:event_btnDangNhapActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
         // TODO add your handling code here:
         System.exit(0);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btnExitActionPerformed
+
+    private void ckbShowHideMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ckbShowHideMouseClicked
+        showHideButton();
+    }//GEN-LAST:event_ckbShowHideMouseClicked
+
+    private void ckbShowHideMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ckbShowHideMousePressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ckbShowHideMousePressed
+
+    private void ckbShowHideKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_ckbShowHideKeyPressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ckbShowHideKeyPressed
 
     /**
      * @param args the command line arguments
@@ -242,16 +284,24 @@ public class DangNhapJFrame extends javax.swing.JFrame {
                 if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DangNhapJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DangNhapJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DangNhapJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DangNhapJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DangNhapJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DangNhapJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DangNhapJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DangNhapJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -265,7 +315,8 @@ public class DangNhapJFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnDangNhap;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnExit;
+    private javax.swing.JCheckBox ckbShowHide;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
